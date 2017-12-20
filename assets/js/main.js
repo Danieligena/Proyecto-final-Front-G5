da523990e4c09c7a6c40c871bed534bc4524d445
danieligena <danieligena@gmail.com>
2017-12-20 01:09:17 -0300

    home minor details - start with both menus and slick

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
+	//NAV - Entrega la clase ACTIVE al hacer click 
 	$(".nav a").on("click", function(){
 		$(".nav").find(".active").removeClass("active");
 		$(this).parent().addClass("active");
 	});
+
+	//NAV LOGO - cambia el logo según el ancho
 
 	var mediaquery = window.matchMedia("(max-width: 768px)");
 	var logo2 = document.querySelector('.ss-logo2');
@@ -28,10 +31,55 @@ $(document).ready(function(){
 
 	mediaquery.addListener(navbarlogo);
 
+	// SLIDER - velocidad del carrousel
+
 	$('.carousel').carousel({
    		interval: 2500,
    		pause: null
   	});
 
+  	// SIDEBAR - start
+
+  	$('.sidebar-btn').click(function(){
+  		$('.sidebar').toggleClass('sidebar-active');
+  		$(this).children().first().toggleClass('toggle');
+  	})
+
+	// JQUERY PLUGIN - llamado a smoothScroll
+
   	$('html').smoothScroll(300);
+
+  	// JQUERY PLUGIN - llamado a slick
+
+  	$('.fotolito').slick({
+		dots: false,
+		infinite: false,
+		speed: 300,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true,
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
 });
